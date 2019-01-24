0c6ae8be92836f62bbdd4eb68b3ed54b0d2541db
Josep Viciana <emmgfx@gmail.com>
2019-01-24 17:45:52 +0100

    Test standalone

---
+++ standalone/README.md
@@ -0,0 +1,6 @@
+# babel-standalone
+
+## Note that:
+
+1. The `<script>` inline or external, written in ES6, are `text/babel` instead of `text/javascript`.
+2. Must define `window.exports = {};` (line 4).
---
+++ standalone/module.js
@@ -0,0 +1,12 @@
+export class Test {
+    
+    constructor(){
+        this.a = 13;
+        this.b = 37;
+    }
+    
+    concat(){
+        return this.a.toString() + this.b.toString();
+    }
+    
+}
